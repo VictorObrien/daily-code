1a5ffff6149b656d91c6323ac584b3b0dc3d0f0f
VictorObrien <victorobrien@hotmail.com>
2021-02-02 16:06:53 -0300

    Inicio do arquivo script.js

---
+++ 02-border-radius-previewer/src/script.js
@@ -0,0 +1,11 @@
+const left_top_value = document.getElementById("left-top")
+const right_top_value = document.getElementById("right-top")
+const left_bottom_value = document.getElementById("left-bottom")
+const right_bottom_value = document.getElementById("right-bottom")
+
+
+
+function imprime(value){
+  value = left_top_value.value
+  return console.log(value)
+}
